9140f3b9b0b8768f9f72ac830125c507a03b3199
vincenzovolpe <vinci792010@gmail.com>
2019-11-22 00:10:49 +0100

    Aggiustata la logica del gioco, corretto funzionamento per uscire dal while

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var minimo; // numero minimo da inserire
 var massimo; // numero massimo da inserire
 var puntiutente = 0; // Variabile che memorizza i punti utente
 var esitomossa = false; // Variabile che ci indica se abbiamo beccato una mina o meno
-
+var giocate = 0; // // Variabile che conta le giocate effettuate
 // Chiedo all'utente il livello di difficoltà del gioco
 alert('Benvenuto nel Gioco Campo minato');
 var scelta = parseInt(prompt('Scegli il  livello di gioco: (digita 0 = facile; 1 = medio; 2 = difficile)'));
@@ -24,13 +24,13 @@ risultatogioco(esitomossa);
 function risultatogioco(esito) {
     // Verifico l'esito della partita
     if (esitomossa) {
-        alert('Colpito e affondato!!!!....Hai beccato una mina!!!')
+        alert('Hai perso!!!!....Hai beccato una mina!!!')
         console.log('Colpito e affondato!!!!....Hai beccato una mina!!!');
-    } else {
+    } else if (giocate == (massimo - maxcasuali)){
         alert('Complimenti hai vinto la battaglia!!!')
         console.log('Complimenti hai vinto la battaglia!!!');
     }
-        alert('Il tuo punteggio finale è: ' + puntiutente);
+    alert('Il tuo punteggio finale è: ' + puntiutente);
     console.log('Il tuo punteggio finale è: ' + puntiutente);
 }
 // Funzione per la scelta dei livelli del Gioco
@@ -58,17 +58,16 @@ function partita(min, max, mossa) {
     console.log(numerotentativi);
     // numerotentativi = 5;
     console.log('Numero di giocate previste ' + numerotentativi);
-    var giocate = 0; // Variabile che conta le giocate effettuate
     do {
         numeroutente = parseInt(prompt('Inserisci un numero da ' +  min + ' a ' + max));
             if (!numeriutente.includes(numeroutente)) { // Controllo che l'utente non ha inserito di nuovo lo stesso numero
                 numeriutente.push(numeroutente); // Inserisco nell'array il numero digitato dall' utente
                 console.log(numeroutente);
                 if (numericasuali.includes(numeroutente)) {
+                    return mossa = true; // Imposto a vero la variabile che mi dice che ho trovato una mina
+                } else {
                     puntiutente++; // Aggiorno i punti totalizzati dall'utente
                     giocate++; // Aggiorno le giocate fatte dall'utente
-                } else {
-                    return mossa = true; // Imposto a vero la variabile che mi dice che ho trovato una mina
                 }
             } else {
                 alert('Attenzione il numero: ' + numeroutente + 'è già stato inserito')
@@ -76,7 +75,7 @@ function partita(min, max, mossa) {
             }
         console.log('Numero giocata: ' + giocate);
         console.log('Punti utente: ' + puntiutente);
-    } while ((numericasuali.includes(numeroutente)) && (giocate < numerotentativi) && (puntiutente < maxcasuali));
+    } while (giocate < numerotentativi);
     return mossa;
 }
 // Funzione che genera un array di numeri random tra min e max in base al livello scelto dall'utente
